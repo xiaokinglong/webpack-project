c078e20e1bc4497cab9fde76e12f458cc2aea3e5
xiaokinglong <xiaokinglong@gmail.com>
2022-01-22 12:05:50 +0800

    开启本地服务

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+
 module.exports = {
   entry: "./src/main.js", // 入口文件
   output: {
     path: path.resolve(__dirname, "./dist"),
     clean: true
+  },
+  devtool: 'inline-source-map',
+  mode: "development",
+  devServer: { // 开启本地服务
+    static: "./dist"
   },
   plugins:[
     new HtmlWebpackPlugin({
